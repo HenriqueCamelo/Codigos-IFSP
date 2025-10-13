4256d7534a84ab908c9af7ed6081541cfd1a33c9
Henri <161380498+HenriqueCamelo@users.noreply.github.com>
2025-10-13 13:42:22 -0300

    Delete Api e Microsserviços/granja-api/granja-api-dto directory

--- Api e Microsserviços/granja-api/granja-api-dto/src/main/java/br/ifsp/granja/repository/DespesasRepository.java
+++
@@ -1,9 +0,0 @@
-package br.ifsp.granja.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import br.ifsp.granja.model.Despesas;
-
-public interface DespesasRepository extends JpaRepository<Despesas, Long> {
-
-}
